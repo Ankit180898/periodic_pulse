0763b1d3df76b5d2bb95d088f5861da6350b69aa
Ankit180898 <karan180898@gmail.com>
2024-04-29 23:29:12 +0530

    Period initial screen

---
+++ lib/view/period_input_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PeriodInputScreen extends StatelessWidget {
+  const PeriodInputScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
